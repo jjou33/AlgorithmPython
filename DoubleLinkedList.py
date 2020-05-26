5deb63867cf0cb7bd8ab9cbf74850f3a1e5ad06d
jjou33 <zanda33@naver.com>
2020-05-26 23:28:57 +0900

    Double Linked List 실습

---
+++ DoubleLinkedList.py
@@ -0,0 +1,39 @@
+## Double Linked List
+
+class Node:
+    def __init__(self,data, prev=None, next=None):
+        self.prev = prev
+        self.data = data
+        self.next = next
+
+class NodeMgnt:
+    def __init__(self,data):
+        self.head = Node(data)
+        self.tail = self.head
+
+    def insert(self,data):
+        if self.head == '':
+            self.head = Node(data)
+            self.tail = self.head
+        else:
+            node = self.head
+            while node.next:
+                node = node.next
+            newNode = Node(data)
+            node.next = newNode
+            newNode.prev = node
+            self.tail = newNode
+
+
+    def desc(self):
+        node = self.head
+        while node:
+            print(node.data)
+            node = node.next
+
+if __name__ == '__main__' :
+    doubleLinkedList = NodeMgnt(0)
+    doubleLinkedList.desc()
+    for i in range(1,10):
+        doubleLinkedList.insert(i)
+    doubleLinkedList.desc()
