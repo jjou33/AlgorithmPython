15957d20eae65af934a364cc92f5272dcc20ba6d
jjou33 <zanda33@naver.com>
2020-05-27 22:20:05 +0900

    DoubleLinkedList 검색 기능 추가

--- DoubleLinkedListSearchNode.py
+++ DoubleLinkedListSearchNode.py
@@ -25,23 +25,34 @@ class NodeMgnt():
             newNode.prev = node
             self.tail = newNode
 
-    def SearchNode(self,data):
+    def SearchNode(self,side,data):
         if self.head == '':
             self.head = Node(data)
             self.tail = self.head
             print("data가 없어 해당 데이터를 첫 데이터로 삽입하였습니다.")
             return
-        else:
+        if side == "next":
             node = self.head
             while node:
                 if node.data == data:
-                    print("입력하신 데이터는 ",  (self.count + 1),  "번째 데이터 입니다.")
+                    print("입력하신 데이터는 앞쪽부터 ", (self.count + 1), "번째 데이터 입니다.")
                     self.count = 0
                     return
                 else:
-                    print("Node.data >>", node.data , "Count >>", self.count)
                     node = node.next
                     self.count += 1
+        else:
+            node = self.tail
+            while node:
+                if node.data == data:
+                    print("입력하신 데이터는 뒷쪽부터 ", (self.count + 1), "번째 데이터 입니다.")
+                    self.count = 0
+                    return
+                else:
+                    node = node.prev
+                    self.count += 1
+
+
     def desc(self):
         node = self.head
         while node:
@@ -54,6 +65,6 @@ if __name__ == '__main__':
     for i in range(1,10):
         testLinkedList.insert(i)
     testLinkedList.desc()
-    testLinkedList.SearchNode(4)
-    testLinkedList.SearchNode(0)
-    testLinkedList.SearchNode(9)
+    testLinkedList.SearchNode("next", 4)
+    testLinkedList.SearchNode("prev", 0)
+    testLinkedList.SearchNode("prev", 9)
