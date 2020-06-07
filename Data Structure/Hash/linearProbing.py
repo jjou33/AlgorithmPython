0a1c42736835be80b500c49d0d12db40678700d9
jjou33 <zanda33@naver.com>
2020-06-07 23:10:21 +0900

    linear Probing 방법론 (Hash)

--- Data Structure/Hash/linearProbing.py
+++ Data Structure/Hash/linearProbing.py
@@ -6,49 +6,53 @@
 1. 해쉬 함수: key % 8
 2. 해쉬 키 생성: hash(data)'''
 
-hash_table = list([0 for i in range(8)])
+class linearProbingHash:
+    def __init__(self,size):
+        self.hash_table = list([0 for i in range(size)])
+        self.size = size
 
+    def get_key(self,data):
+        return hash(data)
 
-def get_key(data):
-    return hash(data)
+    def hash_function(self,key):
+        return key % self.size
 
+    def save_data(self,data,value):
+        index_key = self.get_key(data)
+        hash_address = self.hash_function(index_key)
+        if self.hash_table[hash_address] != 0:
+            for index in range(hash_address, len(self.hash_table)):
+                if self.hash_table[index] == 0:
+                    self.hash_table[hash_address] = [index_key, value]
+                    return
+                elif self.hash_table[index][0] == index_key:
+                    self.hash_table[hash_address][1] = value
+                    return
+                # else:
+                #     self.size += 1
+                #     self.hash_table[self.size].append([index_key, value])
+                #     return
+        else:
+            self.hash_table[hash_address] = [index_key, value]
 
-def hash_function(key):
-    return key % 8
+    def read_data(self,data):
+        index_key = self.get_key(data)
+        hash_address = self.hash_function(index_key)
+        if self.hash_table[hash_address] != 0:
+            for index in range(hash_address, len(self.hash_table)):
+                if self.hash_table[index] == 0:
+                    return None
+                elif self.hash_table[index][0] == index_key:
+                    return self.hash_table[index][1]
+        else:
+            return None
 
-
-def save_data(data, value):
-    index_key = get_key(data)
-    hash_address = hash_function(index_key)
-    if hash_table[hash_address] != 0:
-        for index in range(hash_address, len(hash_table)):
-            if hash_table[index] == 0:
-                hash_table[index] = [index_key, value]
-                return
-            elif hash_table[index][0] == index_key:
-                hash_table[index][1] = value
-                return
-    else:
-        hash_table[hash_address] = [index_key, value]
-
-
-def read_data(data):
-    index_key = get_key(data)
-    hash_address = hash_function(index_key)
-
-    if hash_table[hash_address] != 0:
-        for index in range(hash_address, len(hash_table)):
-            if hash_table[index] == 0:
-                return None
-            elif hash_table[index][0] == index_key:
-                return hash_table[index][1]
-    else:
-        return None
 
 if __name__ == '__main__':
+    hash_table = linearProbingHash(8)
     print(hash('dk') % 8)
     print(hash('da') % 8)
     print(hash('dc') % 8)
-    save_data('dk', '01200123123')
-    save_data('da', '3333333333')
-    print(read_data('dc'))
+    hash_table.save_data('dk', '01200123123')
+    hash_table.save_data('da', '3333333333')
+    print(hash_table.read_data('dk'))
